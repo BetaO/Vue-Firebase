184f7d02cab4d3e7f27d740821942522f0945aa1
Oualid Betari <beta.oualid@gmail.com>
2023-05-18 17:59:03 +0100

    Tutorial vue

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <!-- all Modal template will be nested here -->
+    <!-- passing props header, text, theme to modal -->
+    <!-- receiving event close from modal -->
+    <!-- slots used to pass templates (too complicated for props) / you can use named slots, useful when you have a certain structure -->
+    <Modal :header="header" :text="text" theme="dark" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">like & subscribe</a>
+      </template>
+      <h1>Vue ninja</h1>
+      <p>Available on youtube</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">open modal(alt)</button>
+  <input type="text" ref="name">
+  <button @click="handleclick">click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Modal component
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My first Vue App :)',
+      header: 'Sign up for the Patreon!',
+      text: 'Grab more content',
+      showModal: false
+    }
+  },
+  methods: {
+    handleclick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- event modifier self only applies on backdrop -->
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ dark: theme === 'dark' }">
+            <h1>{{ header }}</h1>
+            <p>pass element from parent with props - {{ text }}</p>
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['header', 'text', 'theme'],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1 {
+        color: aqua;
+        border: none;
+        padding: 0;
+    }
+    .modal.dark{
+        background: crimson;
+        color: white;
+    }
+    .modal.dark.h1{
+        color: white;
+    }
+</style>
